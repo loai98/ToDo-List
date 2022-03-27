d95a4aa33a9fa597c9a47c16c5bf939d7a614206
Loai <loai@sprintive.com>
2022-03-27 18:12:38 +0300

    push and get data from firebase, create drawable for checked radio button, set CheckBox priority color

--- app/src/main/java/com/example/todolist/View/DashboardF.java
+++ app/src/main/java/com/example/todolist/View/DashboardF.java
@@ -1,13 +1,21 @@
 package com.example.todolist.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.todolist.Model.ToDoItem;
@@ -32,13 +40,11 @@ public class DashboardF extends AppCompatActivity {
     FloatingActionButton btn_add;
     FirebaseConnection firebaseConnection;
     public  static  Adapter adapter ;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_f);
-
-
 
         btn_add = findViewById(R.id.btn_add) ;
         recyclerView = findViewById(R.id.recyclerView);
@@ -50,12 +56,39 @@ public class DashboardF extends AppCompatActivity {
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ShowDialog();
+            }
+       });
+    }
+  private   void ShowDialog(){
 
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(DashboardF.this);
+        alertDialog.setTitle(R.string.add_task);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View add_task = inflater.inflate(R.layout.new_item, null);
+        EditText editText=add_task.findViewById(R.id.edittext);
+        RadioGroup radioGroup = add_task.findViewById(R.id.radio_group);
+        alertDialog.setView(add_task) ;
+        alertDialog.setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String title = editText.getText().toString();
+                View checked_btn = add_task.findViewById(radioGroup.getCheckedRadioButtonId());
+                String priority = "0" ;
+                if( !(checked_btn == null || checked_btn.getTag().toString().isEmpty() )){
+                    priority =checked_btn.getTag().toString() ;
+                }
+                Long tsLong = System.currentTimeMillis()/1000;
+                ToDoItem toDoItem = new ToDoItem(tsLong.toString(), title, priority);
+                firebaseConnection.Post(toDoItem);
             }
         });
-
-
+        alertDialog.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        alertDialog.show();
     }
-
 
 }
--- app/src/main/java/com/example/todolist/ViewModel/Adapter.java
+++ app/src/main/java/com/example/todolist/ViewModel/Adapter.java
@@ -1,8 +1,15 @@
 package com.example.todolist.ViewModel;
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.todolist.Model.ToDoItem;
@@ -28,7 +35,15 @@ public class Adapter extends RecyclerView.Adapter<ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.checkBox.setText(toDoItem.get(position).getName());
+        String priority_id = toDoItem.get(position).getPriority();
+        ColorStateList colorStateList = ColorStateList.valueOf(Color.rgb(92, 175, 82));
 
+        if(toDoItem.get(position).getPriority().equals("1")){
+            colorStateList = ColorStateList.valueOf(Color.rgb(34, 150, 243));
+        }else if(toDoItem.get(position).getPriority().equals("2")){
+            colorStateList = ColorStateList.valueOf(Color.rgb(233, 81, 68));
+        }
+holder.checkBox.setButtonTintList(colorStateList);
 
     }
 
